37c7082a4df6eafcf2ae4e11312027a67217826a
madrabit <madrabot@gmail.com>
2020-09-29 10:12:51 +0300

    добавил Liquibase

--- src/main/java/ru/job4j/forum/Main.java
+++ src/main/java/ru/job4j/forum/Main.java
@@ -1,21 +1,29 @@
 package ru.job4j.forum;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
-/**
- * @author madrabit on 25.09.2020
- * @version 1$
- * @since 0.1
- */
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Main.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
     }
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("dataSource") DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -8,3 +8,4 @@ create table posts
 
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
+
